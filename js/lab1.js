eb436d6bbc0a4ef9d640611a137e0b3e7d90961a
PLEASURE <143334418+bobi2077@users.noreply.github.com>
2025-11-26 11:10:36 +0200

    1

--- js/lab1.js
+++ js/lab1.js
@@ -30,27 +30,18 @@ function showListCode() {
         </p>
         <pre>
 &lt;ul&gt;
-    &lt;li&gt;Фантастика&lt;/li&gt;
-    &lt;li&gt;Драма&lt;/li&gt;
-    &lt;li&gt;Бойовик&lt;/li&gt;
+    
 &lt;/ul&gt;
         </pre>
-
         <h3>2. Нумерований список</h3>
         <p>
             Створюється тегом <b>&lt;ol&gt;</b> (ordered list).  
             Атрибути:  
         </p>
         <ul>
-            <li><b>type</b> — стиль нумерації (<code>1</code>, <code>A</code>, <code>a</code>, <code>I</code>, <code>i</code>).</li>
-            <li><b>start</b> — з якого числа почати.</li>
-            <li><b>reversed</b> — відлік у зворотному порядку.</li>
         </ul>
         <pre>
-&lt;ol type="I" start="3"&gt;
-    &lt;li&gt;Фантастика&lt;/li&gt;
-    &lt;li&gt;Драма&lt;/li&gt;
-    &lt;li&gt;Бойовик&lt;/li&gt;
+&lt;ol&gt;
 &lt;/ol&gt;
         </pre>
 
@@ -60,13 +51,9 @@ function showListCode() {
             Елементи:  
         </p>
         <ul>
-            <li><b>&lt;dt&gt;</b> — термін (назва).</li>
-            <li><b>&lt;dd&gt;</b> — його опис.</li>
         </ul>
         <pre>
 &lt;dl&gt;
-    &lt;dt&gt;Фантастика&lt;/dt&gt;
-    &lt;dd&gt;Жанр, що описує вигадані світи та події.&lt;/dd&gt;
 &lt;/dl&gt;
         </pre>
     `;
