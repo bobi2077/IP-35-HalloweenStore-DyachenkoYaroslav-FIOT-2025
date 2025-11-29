074f4e427ce1ad9c1f38fb733f7a847b804affb9
PLEASURE <143334418+bobi2077@users.noreply.github.com>
2025-11-29 12:17:04 +0200

    444

---
+++ js/lab4.js
@@ -0,0 +1,94 @@
+function showDetails5() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> ОСНОВИ JAVASCRIPT У HTML-ДОКУМЕНТІ. ШАБЛОНІ РЯДКИ. КЕРУВАННЯ ПОРЯДКОМ ОБЧИСЛЕНЬ. МАСИВИ. МЕТОДИ МАСИВІВ. ФУНКЦІЇ</p>
+<p><strong>Мета:</strong> ⎯	придбати практичні навички роботи з конструкціями мови JS, масивами та фугкціями у js-сценаріях.  Реалізація програм засовами мови JAVASCRIPT</p>
+<p><strong>Місце розташування:</strong></p>
+<ul>
+        <a href="https://github.com/bobi2077/IP-35_appWEB-DYACHENKOYAROSLAV-FIOT-2025" target="_blank">посилання на репозиторій веб застосунка</a><br>
+        <a href="https://bobi2077.github.io/IP-35_appWEB-DYACHENKOYAROSLAV-FIOT-2025/" target="_blank">посилання на живу сторінку веб-застосунка</a><br>
+        <a href="#" target="_blank">посилання на живу сторінку звітного html-докумсенту</a>
+</ul>`
+;}
+
+function Task1() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./pics/t1.jpg" alt="Placeholder Image" width="700">
+            <img src="./pics/t11.jpg" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./pics/t2.jpg" alt="Placeholder Image" width="700">
+            <img src="./pics/t22.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+    function Task3() {
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = `
+                <img src="./pics/t3.jpg" alt="Placeholder Image" width="700">
+                <img src="./pics/t33.jpg" alt="Placeholder Image" width="700">`
+                
+            ;
+        }
+        function Task4() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+                    <img src="./pics/t4.jpg" alt="Placeholder Image" width="700">
+                    <img src="./pics/t44.jpg" alt="Placeholder Image" width="700">
+                   `
+                ;
+            }
+            function Task5() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./pics/t5.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./pics/t55.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+
+                 function Task6() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./pics/t6.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./pics/t66.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                   function Task7() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./pics/t7.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./pics/t77.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                    function Task8() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./pics/t8.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./pics/t88.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                    function Task9() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./pics/t9.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./pics/t99.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+
+            function showConclusion5() {
+                var outputDiv = document.getElementById("output");
+                var conclusion = `
+                    <p><strong>Висновок:</strong> Під час виконання ЛР №4 я придбав практичні навички роботи з конструкціями мови JS, масивами та функціями у js-сценаріях. Також реалізував програми засовами мови JAVASCRIPT</p>`
+                ;
+                outputDiv.innerHTML = conclusion;
+            }      
