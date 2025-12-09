a5fee37ffbe7d3f53f524d3707d7962d7daab39a
PLEASURE <143334418+bobi2077@users.noreply.github.com>
2025-12-09 17:56:33 +0200

    1

---
+++ js/lab5.js
@@ -0,0 +1,64 @@
+function showDetails5() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> ОСНОВИ JAVASCRIPT У HTML-ДОКУМЕНТІ. ШАБЛОНІ РЯДКИ. КЕРУВАННЯ ПОРЯДКОМ ОБЧИСЛЕНЬ. МАСИВИ. МЕТОДИ МАСИВІВ. ФУНКЦІЇ</p>
+<p><strong>Мета:</strong> ⎯	придбати практичні навички роботи з конструкціями мови JS, масивами та фугкціями у js-сценаріях.  Реалізація програм засовами мови JAVASCRIPT</p>
+<p><strong>Місце розташування:</strong></p>
+<ul>
+        <a href="https://github.com/bobi2077/IP-35_appWEB-DYACHENKOYAROSLAV-FIOT-2025" target="_blank">посилання на репозиторій веб застосунка</a><br>
+        <a href="https://bobi2077.github.io/IP-35_appWEB-DYACHENKOYAROSLAV-FIOT-2025/" target="_blank">посилання на живу сторінку веб-застосунка</a><br>
+        <a href="#" target="_blank">посилання на живу сторінку звітного html-докумсенту</a>
+</ul>`
+;}
+
+function Task2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./pics/task2.jpg" alt="Placeholder Image" width="700">
+            <img src="./pics/task22.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+    
+        function Task4() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+                    <img src="./pics/task4.jpg" alt="Placeholder Image" width="700">
+                    <img src="./pics/task44.jpg" alt="Placeholder Image" width="700">`
+                ;
+            }
+            
+
+                 function Task6() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./pics/task6.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./pics/task66.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                 
+                    function Task8() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./pics/task8.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./pics/task88.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                function Task10() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./pics/task10.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./pics/task1010.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                    
+
+            function showConclusion5() {
+                var outputDiv = document.getElementById("output");
+                var conclusion = `
+                    <p><strong>Висновок:</strong> Під час виконання ЛР №5 я придбав практичні навички роботи з конструкціями мови JS, масивами та функціями у js-сценаріях. Також реалізував програми засовами мови JAVASCRIPT</p>`;
+                outputDiv.innerHTML = conclusion;
+            }      
