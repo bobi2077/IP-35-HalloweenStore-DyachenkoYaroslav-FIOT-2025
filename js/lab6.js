5c9ea329ccc6c1268d60e7538e62b7c876a58d74
PLEASURE <143334418+bobi2077@users.noreply.github.com>
2025-12-15 19:49:59 +0200

    oyoy

---
+++ js/lab6.js
@@ -0,0 +1,70 @@
+function showDetails() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> ПРОТОТИПИ. КЛАСИ. ОБ'ЄКТНА МОДЕЛЬ ДОКУМЕНТА (DOM). ПОДІЇ. ОБ'ЄКТ ПОДІЇ. ДЕЛЕГУВАННЯ ПОДІЙ. </p>
+<p><strong>Мета:</strong> придбати практичні навички роботи з DOM, подіями, делегуванням подій</p>
+<strong>Місце розташування:</strong> 
+<ul>
+        <a href="https://github.com/bobi2077/IP-35_appWEB-DYACHENKOYAROSLAV-FIOT-2025" target="_blank">посилання на репозиторій веб застосунка</a><br>
+        <a href="https://bobi2077.github.io/IP-35_appWEB-DYACHENKOYAROSLAV-FIOT-2025/" target="_blank">посилання на живу сторінку веб-застосунка</a><br>
+        <a href="#" target="_blank">посилання на живу сторінку звітного html-докумсенту</a>
+</ul>`
+;}
+
+function Task2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+       <li><a href="./lab6/task2.html">Завдання 2</a></li>
+            <img src="./lab6/pics/t2.png" alt="Placeholder Image" width="700">
+            <img src="./lab6/pics/c2.png" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task4() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+    <li><a href="./lab6/task4.html">Завдання 4</a></li>
+            <img src="./lab6/pics/t4.png" alt="Placeholder Image" width="700">
+            <img src="./lab6/pics/c4.png" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+    function Task6() {
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = `
+        <li><a href="./lab6/task6.html">Завдання 6</a></li>
+                <img src="./lab6/pics/t6.png" alt="Placeholder Image" width="700">
+                <img src="./lab6/pics/c6.png" alt="Placeholder Image" width="700">`
+                
+            ;
+        }
+        
+        function Task8() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+            <li><a href="./lab6/task8.html">Завдання 8</a></li>
+                    <img src="./lab6/pics/t8.jpg" alt="Placeholder Image" width="700">
+                    <img src="./lab6/pics/t88.png" alt="Placeholder Image" width="700">
+                    <img src="./lab6/pics/c8.png" alt="Placeholder Image" width="700">`
+                ;
+            }
+            
+        function Task10() {
+                    var outputDiv = document.getElementById("output");
+                    outputDiv.innerHTML = `
+                    <li><a href="./lab6/task10.html">Завдання 10</a></li>
+                            <img src="./lab6/pics/t10.png" alt="Placeholder Image" width="700">
+                        
+                            <img src="./lab6/pics/c10.png" alt="Placeholder Image" width="700">`
+                        ;
+            }
+
+            function showConclusion() {
+                var outputDiv = document.getElementById("output");
+                var conclusion = `
+                    <p><strong>Висновок:</strong> Під час виконання ЛР №6 я придбав практичні навички роботи з об'єктами. Методи об'єкта.. Callback. Стрілочні функції. Стрілочні функції як колбеки.
+.  Реалізація програм засовами мови JAVASCRIPT
+</p>`
+                ;
+                outputDiv.innerHTML = conclusion;
+            }  
